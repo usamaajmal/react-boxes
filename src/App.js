66ff43e61050cfa14d5b2bfd7c7fc62409498cdb
usamaajmal <usamaajmal@gmail.com>
2022-02-22 07:34:24 -0500

    changes for diagnols

--- src/App.js
+++ src/App.js
@@ -8,16 +8,53 @@ function App() {
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
   ];
   //const [box, setBox]=useState([[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]);
   const [arr, setBox] = useState(initialValue);
   const [update, setUpdate]=useState(true);
-  const updateColorValue = (row, col, value) => {
-    arr[row][col] = value;
-    setBox(arr);
-    setUpdate(!update);
+  let [first, setFirst] = useState({
+    row: 0, col: 0
+  })
+  let [prev, setPrev] = useState({
+    row: -1, col: -1
+  })
+  const updateColorValue = (row, col) => {
+    let value
+  console.log("current",prev, row,col)
+ 
+    let rowInitial = first.row < row ? first.row : row
+    let rowEnd = first.row >= row ? first.row : row
+    let colInitial = first.col < col ? first.col : col
+    let colEnd = first.col >= col ? first.col : col
+    setBox(initialValue)
+    // console.log(prev, first, {row,col})
+    if(((prev.row !== row || prev.column !== col))&&(row===col)){
+      for(let x = rowInitial; x<= rowEnd; x++){
+        for(let y = colInitial; y <= colEnd ; y++){
+          value = arr[x][y] === 0 ? 1 : 0;
+          value = 1
+          arr[x][y] = value;
+          console.log(x,y,arr[x][y])
+        }
+      }
+      prev.row = row
+      prev.col = col
+      setPrev(prev)
+      // console.log("prev", prev)
+      setBox(arr);
+      setUpdate(!update);
+
+    }
   };
 
+
+  const setFirstValue = (row, col) => {
+    first.row = row;
+    first.col = col
+    setFirst(first)
+  
+  }
 
   const reset=()=>{
     setBox(initialValue);
@@ -33,6 +70,7 @@ function App() {
             rowIndex={index}
             key={index}
             updateColorValue={updateColorValue}
+            setFirstValue = {setFirstValue}
             reset={reset}
           />
         );
--- src/components/Box.js
+++ src/components/Box.js
@@ -3,13 +3,17 @@ import React from 'react';
 export default function Box(props) {  
     
  const changeColor = ()=>{
-     props.updateColorValue(props.rowIndex, props.colIndex,1);
+     props.updateColorValue(props.rowIndex, props.colIndex);
      
+ }
+ 
+ const first = () => {
+   props.setFirstValue(props.rowIndex, props.colIndex)
  }
  
   return (
       <>
-       <div style={(props.colorValue === 1)? ({backgroundColor:"red"}):{backgroundColor:"green"}} className="box" draggable onDragEnd={props.reset}  onDragOver={changeColor}   ></div>
+       <div style={(props.colorValue === 1)? ({backgroundColor:"red"}):{backgroundColor:"green"}} className="box" draggable onDragStart={first} onDragEnd={props.reset}  onDragOver={changeColor}   ></div>
        
 </>
   )
